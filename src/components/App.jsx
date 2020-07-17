f203cf618720fae12b4b80fbe3e46d2b740bf852
dimapanasiuk <dimapanasiuk@icloud.com>
2020-07-17 14:27:52 +0300

    add test for react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import './appPerproc.scss'
 import Logo from './img/1.jpg';
+import Button from './button/Button.jsx';
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Button label="click me please"/>
         <h1>My React App!</h1>
-        <img src={Logo} />
+        {/* <img src={Logo} /> */}
       </div>
     );
   }
